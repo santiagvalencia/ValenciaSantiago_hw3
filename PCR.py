e727a077d6f71a32e434d4bbb76854f6f89b4161
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-22 13:10:21 -0500

    PCR.py: altera la matriz de datos para que B y M se reemplacen por 1 y 0, respectivamente

--- PCR.py
+++ PCR.py
@@ -2,3 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos = np.genfromtxt("WDBC.dat", delimiter = ",")
+datos_str = np.genfromtxt("WDBC.dat", delimiter = "," , dtype = 'str', usecols = (1))
+
+for i in range(datos_str.size):
+    if datos_str[i] == "B":
+        datos[i, 1] = 1
+    else: datos[i, 1] = 0
