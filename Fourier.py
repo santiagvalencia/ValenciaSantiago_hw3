d625230a10bc15a416e5fa16a242b754e036df88
Santiago Valencia <santiagovalencia@192.168.1.100>
2018-10-21 21:04:08 -0500

    Fourier.py: reorganiza el codigo con funciones para la transformada de Fourier (FT), la transformada inversa (IFT) y la obtencion de las frecuencias (frecuencias)

--- Fourier.py
+++ Fourier.py
@@ -16,7 +16,7 @@ plt.close("all")
 
 dt = np.mean(signal_t[1:]-signal_t[:-1])
 
-def FT(g, dt):
+def FT(g):
     N = g.size
     n = N
     k = N
@@ -26,7 +26,9 @@ def FT(g, dt):
         for ki in range(k):
             sum += g[ki]*np.exp(1j*2.0*np.pi*ki*ni/N)
         G[ni] = sum
+    return G
 
+def frecuencias(n, dt):
     freq = np.zeros(n)
     if n%2 == 0:
         c = -n/2
@@ -44,40 +46,48 @@ def FT(g, dt):
 
     freq/=(dt*n)
 
-    print("\nNo se usa el paquete fftfreq (bono)\n")
-    plt.plot(freq, np.real(G))
-    plt.title("Transformada de Fourier de signal.dat")
-    plt.xlabel("Frecuencia (Hz)")
-    plt.ylabel("Transformada de Fourier")
-    plt.savefig("ValenciaSantiago_TF.pdf")
-    plt.close("all")
+    return freq
 
-    print("Frecuencias principales:")
-    G_real = np.real(G)
-    for i in range(G_real.size):
-        if np.fabs(G_real[i]) >= 200 and freq[i]>=0:
-            print (freq[i], " Hz")
-
-    G2 = np.copy(G)
-    for i in range(freq.size):
-        if np.fabs(freq[i]) >= 1000:
-            G2[i] = 0
-
-    g2 = np.zeros(N) + 0j
+def IFT(G):
+    N = G.size
+    n = N
+    k = N
+    g = np.zeros(N) + 0j
     for ni in range(n):
         sum = 0
         for ki in range(k):
-            sum += G2[ki]*np.exp(-1j*2.0*np.pi*ki*ni/N)
-        g2[ni] = sum
-    g2/=n
+            sum += G[ki]*np.exp(-1j*2.0*np.pi*ki*ni/N)
+        g[ni] = sum
+    g/=n
+    return g
 
-    plt.plot(signal_t, np.real(g2))
-    plt.xlabel("Tiempo (s)")
-    plt.ylabel("Amplitud")
-    plt.title("Datos filtrados de signal.dat")
-    plt.savefig("ValenciaSantiago_filtrada.pdf")
-    plt.close("all")
+G = FT(signal_Amplitud)
+freq = frecuencias(signal_Amplitud.size, dt)
 
+print("\nNo se usa el paquete fftfreq (bono)\n")
+plt.plot(freq, np.real(G))
+plt.title("Transformada de Fourier de signal.dat")
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Transformada de Fourier")
+plt.savefig("ValenciaSantiago_TF.pdf")
+plt.close("all")
 
+print("Frecuencias principales:")
+G_real = np.real(G)
+for i in range(G_real.size):
+    if np.fabs(G_real[i]) >= 200 and freq[i]>=0:
+        print (freq[i], " Hz")
 
-FT(signal_Amplitud, dt)
+G2 = np.copy(G)
+for i in range(freq.size):
+    if np.fabs(freq[i]) >= 1000:
+        G2[i] = 0
+
+g2 = IFT(G2)
+
+plt.plot(signal_t, np.real(g2))
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Amplitud")
+plt.title("Datos filtrados de signal.dat")
+plt.savefig("ValenciaSantiago_filtrada.pdf")
+plt.close("all")
