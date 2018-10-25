962b42c64e0ccc60bf38912a559de29e8496d0ec
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-24 21:38:57 -0500

    Imprime los componentes principales despues de ordenar los vectores propios segun sus valores propios correspondientes, de mayor a menor

--- PCR.py
+++ PCR.py
@@ -11,7 +11,10 @@ for i in range(diagnosticos_str.size):
     else:
         datos[i, 1] = 0
 
-parametros = datos[:,2:]
+parametros = datos[:,1:]
+
+for i in range(parametros[0].size):
+    parametros[:, i] -= np.mean(parametros[:, i])
 
 N = parametros[0].size #numero de parametros (sin numero de indentificacion ni diagnostico)
 
@@ -25,9 +28,28 @@ for i in range(N):
         Mcov[i, j] = cov(parametros[:, i], parametros[:, j])
 print(Mcov)
 
+numeros = np.arange(1, N+1)
 eigValues, eigVectors = np.linalg.eig(Mcov)
 
+print("\n")
 for i in range(eigValues.size):
     print("valor propio",i+1, ":", eigValues[i])
     print("vector propio",i+1,":", eigVectors[i])
     print("\n")
+
+#ordena valores propios y sus correspondientes vectores propios
+for i in range(eigValues.size):
+    for j in range(eigValues.size):
+        if(eigValues[j]<eigValues[i]):
+            temp = eigValues[i]
+            temp_vect = eigVectors[i]
+            temp_num = numeros[i]
+            eigValues[i] = eigValues[j]
+            eigVectors[i] = eigVectors[j]
+            numeros[i] = numeros[j]
+            eigValues[j] = temp
+            eigVectors[j] = temp_vect
+            numeros[j] = temp_num
+
+print("Componente principal 1: vector", numeros[0], "= ", eigVectors[0], " correspondiente al valor propio", eigValues[0])
+print("Componente principal 2: vector", numeros[1], "= ", eigVectors[1], " correspondiente al valor propio", eigValues[1])
