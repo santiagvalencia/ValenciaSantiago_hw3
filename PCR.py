c731f6e6ab024ea97215e42391d98cdb9f74d903
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-24 20:05:08 -0500

    Nueva matriz parametros que solo guarda los parametros de diagnostico. Nueva funcion cov que calcula la covarianza entre dos arreglos. Nueva matriz Mcov, la matriz de covarianza entre los datos.

--- PCR.py
+++ PCR.py
@@ -2,9 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos = np.genfromtxt("WDBC.dat", delimiter = ",")
-datos_str = np.genfromtxt("WDBC.dat", delimiter = "," , dtype = 'str', usecols = (1))
+diagnosticos_str = np.genfromtxt("WDBC.dat", delimiter = "," , dtype = 'str', usecols = (1))
+diagnosticos = np.zeros(diagnosticos_str.size)
+for i in range(diagnosticos_str.size):
+    if diagnosticos_str[i] == "B":
+        diagnosticos[i] = 1
 
-for i in range(datos_str.size):
-    if datos_str[i] == "B":
-        datos[i, 1] = 1
-    else: datos[i, 1] = 0
+parametros = datos[:,2:]
+
+N = parametros[0].size #numero de parametros (sin numero de indentificacion ni diagnostico)
+
+Mcov = np.zeros((N, N))
+
+def cov(x, y):
+    return np.sum((x-np.mean(x))*(y-np.mean(y)))/(x.size-1)
+
+for i in range(N):
+    for j in range(N):
+        Mcov[i, j] = cov(parametros[:, i], parametros[:, j])
+
+
+print(Mcov)
