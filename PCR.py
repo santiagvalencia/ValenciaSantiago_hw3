df1c89d89a04a79ce9eeefa8198380954cbd833c
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-24 23:38:18 -0500

    Crea la matriz de proyeccion con PC1 y PC2 y proyecta los datos sobre estos ejes. Grafica los datos y colorea el diagnostico M con rojo y el B con verde

--- PCR.py
+++ PCR.py
@@ -11,7 +11,7 @@ for i in range(diagnosticos_str.size):
     else:
         datos[i, 1] = 0
 
-parametros = datos[:,1:]
+parametros = datos[:,2:]
 
 for i in range(parametros[0].size):
     parametros[:, i] -= np.mean(parametros[:, i])
@@ -26,6 +26,7 @@ def cov(x, y):
 for i in range(N):
     for j in range(N):
         Mcov[i, j] = cov(parametros[:, i], parametros[:, j])
+print("\nMatriz de covarianza: ")
 print(Mcov)
 
 numeros = np.arange(1, N+1)
@@ -33,23 +34,37 @@ eigValues, eigVectors = np.linalg.eig(Mcov)
 
 print("\n")
 for i in range(eigValues.size):
-    print("valor propio",i+1, ":", eigValues[i])
-    print("vector propio",i+1,":", eigVectors[i])
-    print("\n")
+    if i == 0 or i == 1:
+        print("valor propio",i+1, ":", eigValues[i])
+        print("vector propio",i+1,":", eigVectors[i])
+        print("\n")
 
 #ordena valores propios y sus correspondientes vectores propios
 for i in range(eigValues.size):
     for j in range(eigValues.size):
         if(eigValues[j]<eigValues[i]):
             temp = eigValues[i]
-            temp_vect = eigVectors[i]
+            temp_vect = np.copy(eigVectors[i])
             temp_num = numeros[i]
             eigValues[i] = eigValues[j]
-            eigVectors[i] = eigVectors[j]
+            eigVectors[i] = np.copy(eigVectors[j])
             numeros[i] = numeros[j]
             eigValues[j] = temp
             eigVectors[j] = temp_vect
             numeros[j] = temp_num
-
+print("\n")
 print("Componente principal 1: vector", numeros[0], "= ", eigVectors[0], " correspondiente al valor propio", eigValues[0])
 print("Componente principal 2: vector", numeros[1], "= ", eigVectors[1], " correspondiente al valor propio", eigValues[1])
+
+PC1 = eigVectors[0]
+PC2 = eigVectors[1]
+
+Mproy = np.hstack((PC1.reshape(N, 1), PC2.reshape(N, 1)))
+
+Y = np.matmul(parametros, Mproy)
+
+for i in range(Y[:, 0].size):
+    if diagnosticos[i] == 1:
+        plt.scatter(Y[i, 0], Y[i, 1], color = "green")
+    else: plt.scatter(Y[i, 0], Y[i, 1], color = "red")
+plt.show()
