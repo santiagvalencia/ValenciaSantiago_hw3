302d84019d30e4a5ca247ff6aabdd848c98fcd8e
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-26 15:00:41 -0500

    Anade documentacion y mejora la forma de guardar la imagen filtrada

--- Fourier2D.py
+++ Fourier2D.py
@@ -2,30 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from numpy.fft import fft2, ifft2
-
+#Lee la imagen
 img = plt.imread("arbol.png")
-
+#Aplica la transformada de Fourier de la imagen y muestra en un plot su parte real en valor absoluto
 img_FT = fft2(img)
-plt.imshow(np.abs(np.real(img_FT)), norm = LogNorm(vmin = 5), cmap = "hot")
+plt.imshow(np.abs(np.real(img_FT)), norm = LogNorm(vmin = 3), cmap = "hot")
 plt.colorbar(label = "Transformada 2D de Fourier")
 plt.title("Transformada de Fourier de la imagen")
-plt.show()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("ValenciaSantiago_FT2D.pdf")
 plt.close("all")
 
 #el ruido se ve como unos cuantos puntos con valores altos (>10**3.3)
 #por eso el filtro debe ser pasabajas
-img_FT_filtro = np.copy(img_FT)
+img_FT_filtro = np.copy(img_FT)#copia la transformada y la recorre
 for i in range(img_FT_filtro[:, 0].size):
     for j in range(img_FT_filtro[0].size):
-        if np.abs(np.real(img_FT[i, j])) >= 10.0**3.3:
+        if np.abs(np.real(img_FT[i, j])) >= 10.0**3.3: #si el valor de la transformada es mayor a 10**3.3 este valor pasa a ser el promedio de sus vecinos
             img_FT_filtro[i, j] = (img_FT_filtro[i+1, j] + img_FT_filtro[i-1, j] + img_FT_filtro[i, j+1] + img_FT_filtro[i, j-1])/4
-
-plt.imshow(np.abs(np.real(img_FT_filtro)), norm = LogNorm(vmin = 5), cmap = "hot")
+#Guarda el valor absoluto de la parte real de la transformada filtrada
+plt.imshow(np.abs(np.real(img_FT_filtro)), norm = LogNorm(vmin = 3), cmap = "hot")
 plt.colorbar(label = "Transformada 2D de Fourier")
 plt.title("Transformada de Fourier filtrada")
-plt.show()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("ValenciaSantiago_FT2D_filtrada.pdf")
 plt.close("all")
-
+#invierte la transformada de la imagen y guarda el resultado
 img_filtrada = ifft2(img_FT_filtro)
-plt.imshow(np.real(img_filtrada), cmap = "gray")
-plt.show()
+plt.imsave("ValenciaSantiago_Imagen_filtrada.pdf", np.real(img_filtrada), format = "pdf", cmap = "gray")
+plt.close("all")
