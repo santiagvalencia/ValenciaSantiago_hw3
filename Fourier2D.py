4ae4edfd9171ae602b749a620b5d51e4a5462462
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-25 21:35:04 -0500

    Muestra la transformada de Fourier 2D de la imagen

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fftn, ifftn
+from matplotlib.colors import LogNorm
+from numpy.fft import fft2
 
 img = plt.imread("arbol.png")
-plt.imshow(img, cmap = "gray")
+
+img_FT = np.abs(np.real(fft2(img)))
+plt.imshow(img_FT, norm = LogNorm(vmin = 1), cmap = "jet")
+plt.colorbar()
 plt.show()
+
+
+
+#plt.imshow(img, cmap = "gray")
+#plt.show()
