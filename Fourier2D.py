f4492fe11a6ffe9b25d3b0c4c363d3b7b0b5bfef
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-10-30 20:18:48 -0500

    Correcciones

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,32 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from numpy.fft import fft2, ifft2
+from numpy.fft import fft2, ifft2, fftshift
 #Lee la imagen
 img = plt.imread("arbol.png")
 #Aplica la transformada de Fourier de la imagen y muestra en un plot su parte real en valor absoluto
 img_FT = fft2(img)
-plt.imshow(np.abs(np.real(img_FT)), norm = LogNorm(vmin = 3), cmap = "hot")
+plt.imshow(np.abs(np.real(fftshift(img_FT))), norm = LogNorm(vmin = 3), cmap = "hot")
 plt.colorbar(label = "Transformada 2D de Fourier")
 plt.title("Transformada de Fourier de la imagen")
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel("u")
+plt.ylabel("v")
 plt.savefig("ValenciaSantiago_FT2D.pdf")
 plt.close("all")
 
 #el ruido se ve como unos cuantos puntos con valores altos (>10**3.3)
-#por eso el filtro debe ser pasabajas
+#por eso el filtro debe ser pasabandas y que elimine las frecuencias correspondientes a esos picos
 img_FT_filtro = np.copy(img_FT)#copia la transformada y la recorre
 for i in range(img_FT_filtro[:, 0].size):
     for j in range(img_FT_filtro[0].size):
-        if np.abs(np.real(img_FT[i, j])) >= 10.0**3.3: #si el valor de la transformada es mayor a 10**3.3 este valor pasa a ser el promedio de sus vecinos
-            img_FT_filtro[i, j] = (img_FT_filtro[i+1, j] + img_FT_filtro[i-1, j] + img_FT_filtro[i, j+1] + img_FT_filtro[i, j-1])/4
-#Guarda el valor absoluto de la parte real de la transformada filtrada
-plt.imshow(np.abs(np.real(img_FT_filtro)), norm = LogNorm(vmin = 3), cmap = "hot")
+        if np.abs(np.real(img_FT[i, j])) >= 10.0**3.3: #si el valor de la transformada es mayor a 10**3.3 este valor pasa a 0, lo que efectivamente elimina las frecuencias correspondientes a los picos de ruido
+            img_FT_filtro[i, j] = 0
+plt.imshow(np.abs(np.real(fftshift(img_FT_filtro))), norm = LogNorm(vmin = 3), cmap = "hot")
 plt.colorbar(label = "Transformada 2D de Fourier")
-plt.title("Transformada de Fourier filtrada")
-plt.xlabel("x")
-plt.ylabel("y")
+plt.title("Transformada de Fourier filtrada (shifted)")
+plt.xlabel("u")
+plt.ylabel("v")
 plt.savefig("ValenciaSantiago_FT2D_filtrada.pdf")
 plt.close("all")
 #invierte la transformada de la imagen y guarda el resultado
